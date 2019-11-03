5f9fa06f1e9100bded2d95f5646bd3c9af3c6846
Inoue343 <20026379+Inoue343@users.noreply.github.com>
2019-11-03 22:10:30 +0900

    libusb追加.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
+#include <iostream>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <libusb-1.0/libusb.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    auto version = libusb_get_version();
+    std::cout << version->major << '.' << version->minor << '.' << version->micro << std::endl;
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
 
